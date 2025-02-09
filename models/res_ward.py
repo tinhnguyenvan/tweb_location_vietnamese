86a283f4bfe1912c6ede2acb29e4f1424cb660a1
tinh.nguyen <tinhnguyenvan91@gmail.com>
2025-02-09 23:56:27 +0700

    add log

--- models/res_ward.py
+++ models/res_ward.py
@@ -11,13 +11,16 @@ class ResWard(models.Model):
     country_id = fields.Many2one('res.country', string='Country', required=True)
     state_id = fields.Many2one('res.country.state', 'State', domain="[('country_id', '=', country_id)]")
     district_id = fields.Many2one('res.district', 'District', domain="[('state_id', '=', state_id)]")
-    ghn_ward_id = fields.Char("GHN Ward Code", help='Mã phường/xã theo Giao hàng Nhanh')
+    ghn_ward_id = fields.Char("GHN Ward Code", help='Mã phường/xã theo giao hàng nhanh')
 
     def create_ward_data(self):
+        print('START SYNC WARD DATA ------------------------------')
+
         all_district = self.env['res.district'].sudo().search([('source_id', '>', 0)])
         i = 1
-        
+
         for dis in all_district:
+            print('-- source_id: ' + source_id)
             request_url = "https://online-gateway.ghn.vn/shiip/public-api/master-data/ward?district_id="+str(dis.source_id)
             ghn_token = self.env['ir.config_parameter'].sudo().get_param('ghn_token')
             headers = {
@@ -26,11 +29,11 @@ class ResWard(models.Model):
             }
             req = requests.get(request_url, headers=headers)
             req.raise_for_status()
-            content = req.json()    
+            content = req.json()
             data = content['data']
             if data is None:
                 continue
-            
+
             for rec in data:
                 existed_district = self.env['res.district'].sudo().search([('source_id', '=', rec['DistrictID'])],limit=1)
                 if existed_district:
@@ -42,5 +45,5 @@ class ResWard(models.Model):
                     vals['ghn_ward_id'] = rec['WardCode']
                     i += 1
                     self.env['res.ward'].sudo().create(vals)
-                    
-        print('DONE SYNC WARD DATA')
+
+        print('DONE SYNC WARD DATA ---------------------------')
